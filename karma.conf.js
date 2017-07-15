6dd91d0b61edb4a9b8865ad6914f3a457c87cfca
lucascorreia <tsirlucas@gmail.com>
2017-07-15 17:31:05 -0300

    Adding travis-ci and fixing karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -3,27 +3,19 @@ module.exports = function (config) {
         browsers: ['PhantomJS'],
         files: [
             './node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
-            {pattern: 'dist/index.js', included: true, watched: true},
+            {pattern: 'src/index.js', included: true, watched: true},
             {pattern: 'test/**/*.spec.js', watched: true}
         ],
-        frameworks: ['jasmine'],
+        frameworks: ['browserify', 'jasmine'],
         preprocessors: {
-            'src/**/*.js': ['babel'],
-            'test/**/*spec.js': ['babel']
+            'src/**/*.js': ['browserify'],
+            'test/**/*spec.js': ['browserify']
         },
-        babelPreprocessor: {
-            options: {
-                presets: [
-                    "es2015",
-                    "stage-0"
-                ]
-            },
-            filename: function (file) {
-                return file.originalPath.replace(/\.js$/, '.es5.js');
-            },
-            sourceFileName: function (file) {
-                return file.originalPath;
-            }
+        browserify: {
+            debug: true,
+            transform: [
+                ['babelify']
+            ]
         }
     });
 };
