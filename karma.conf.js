928e738a2037c1bd0875018b73880864f5321f5e
lucascorreia <tsirlucas@gmail.com>
2017-07-15 21:03:51 -0300

    Removing trailing comma and adding phantomjs launcher to dev dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,6 @@ module.exports = function (config) {
                 }]
             ]
         },
-        reporters: ['progress', 'coverage'],
+        reporters: ['progress', 'coverage']
     });
 };
